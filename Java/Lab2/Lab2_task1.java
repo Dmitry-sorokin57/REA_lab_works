83a46bd32519d4fd31c842d9ee01d17e39222f8f
Дмитрий Сорокин <74465537+Dmitry-sorokin57@users.noreply.github.com>
2025-10-02 17:37:19 +0300

    Update Lab2_task1.java

--- Java/Lab2/Lab2_task1.java
+++ Java/Lab2/Lab2_task1.java
@@ -1,1 +1,65 @@
+// Структура узла
+class Node {
+    String val;
+    Node next;
 
+    // Конструктор для вызова
+    Node(String _val) {
+        val = _val;
+        next = null;
+    }
+}
+
+// Структура односвязного списка
+class List {
+    Node first;
+    Node last;
+
+    // Базовый конструктор
+    List() {
+        first = null;
+        last = null;
+    }
+
+    // Функция, проверяющая пустоту списка
+    boolean is_empty() {
+        return first == null;
+    }
+
+    // Функция, отвечающая за добавление элемента в конец списка
+    void push_back(String _val) {
+        Node p = new Node(_val);
+        if (is_empty()) {
+            first = p;
+            last = p;
+            return;
+        }
+        last.next = p;
+        last = p;
+    }
+
+    // Функция для печати элементов списка
+    void print() {
+        if (is_empty()) return;
+        Node p = first;
+        while (p != null) {
+            System.out.print(p.val + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        List l = new List();
+        System.out.println(l.is_empty());
+        l.push_back("1");
+        l.push_back("2");
+        l.push_back("3");
+        l.push_back("4");
+        l.push_back("5");
+        System.out.println(l.is_empty());
+        l.print();
+    }
+}
